3cedfb727ebed9d36cc77f45bb500c0f7b1454b8
Victor Ojong <victorojong78@gmail.com>
2024-01-25 21:56:31 +0100

    add: bookings service , create bookings route

--- src/app/bookings/bookings.controller.ts
+++ src/app/bookings/bookings.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateBookingDto } from './dtos/create-bookings.dto';
+import { BookingsService } from './bookings.service';
 
 @Controller('bookings')
-export class BookingsController {}
+export class BookingsController {
+  constructor(private bookingsService: BookingsService) {}
+  @Post('/new')
+  createUser(@Body() body: CreateBookingDto) {
+    return this.bookingsService.createBooking(body);
+  }
+}
