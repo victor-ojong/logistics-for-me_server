6dec2c4b567de6281987f6225c1de7ba1d0a3c36
Victor Ojong <victorojong78@gmail.com>
2024-02-07 15:48:28 +0100

    Configured config files

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const PORT = process.env.PORT || 8000;
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({ keys: ['some-random-string-for-encryption'] }));
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.use(cookieSession({ keys: ['string-for-encryption'] }));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.setGlobalPrefix('api/v1');
   await app.listen(PORT);
 }
